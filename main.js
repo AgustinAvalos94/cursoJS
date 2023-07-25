483b80c45ba04c1491d6a529e2dd21a48344d8a5
Agustin Avalos <agustinavalos0894@gmail.com>
2023-07-24 21:51:54 -0300

    Productos se carga con Fetch desde productos.jason, carrito se sigue trabajando desde el navegador

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
-//Defino una lista de productos inicial y verifico si no esá en local Storage, si hay recupero
+let productos = []
+
+//Cargo los productos mediante FETCH
+cargarProductos()
+  .then(data => {
+    productos = data;
+    renderizar(productos)
+  })
+  .catch(error => {
+    console.error('Error al cargar los productos:', error);
+  });
 
 
-
-let productos = [
-    {id:1, nombre:'Micropolar Alaskan', tipo:'Buzo', actividad:'Trekking', stock: 7, precio: 20000, tec:'BearTech', rutaImagen: "micropolar.jfif",},
-    {id:2, nombre:'Rompevientos North-wind', tipo:'Campera', actividad:'Trekking', stock: 4, precio: 25000, tec:'NoMoreWind', rutaImagen: "softshell.jfif",},
-    {id:3, nombre:'Carpa StarRiver 2P', tipo:'Carpa', actividad:'Camping', stock: 5, precio: 150000, tec:'AquaOut', rutaImagen: "carpa.jpg",},
-    {id:4, nombre:'Crampones Irvix FlexLock', tipo:'Crampones', actividad:'Escalada', stock: 1, precio: 93000, tec:'SteelFlex', rutaImagen: "crampones.jpg",},
-    {id:5, nombre:'Remera Finland', tipo:'Remera', actividad:'Trekking', stock: 10, precio: 30000, tec:'DryFit', rutaImagen: "remera.webp",},
-    {id:6, nombre:'Botas Cruzader', tipo:'Botas', actividad:'Trekking', stock: 2, precio: 65000, tec:'HardGrip', rutaImagen: "botas.webp",},
-]
-let productosJSON = JSON.parse(localStorage.getItem("productos"))
-if(productosJSON){
-    productos=productosJSON
-}
 //Defino Carrito y verifico si hay carrito en local Storage
 let carrito = []
 let carritoJSON = JSON.parse(localStorage.getItem("carrito"))
@@ -134,7 +131,25 @@ function agregarCarrito(e){
                 cantidad: 1
             });
             }
-        localStorage.setItem("carrito",JSON.stringify(carrito))
+
+            //ACTUALIZO EL STOCK DE PRODUCTOS MEDIANTE FETCH
+            fetch('productos.json', {
+                method: 'PUT', 
+                body: JSON.stringify(productos), 
+                headers: {
+                  'Content-Type': 'application/json'
+                }
+              })
+              .then(response => {
+                if (!response.ok) {
+                  throw new Error('La solicitud no fue exitosa al actualizar el stock');
+                }
+                return response.json();
+              })
+              .catch(error => {
+                console.error('Error al actualizar el stock:', error);
+              });
+
         const tarjetaProducto = e.target.parentElement;
         const unidadesDisponibles = tarjetaProducto.querySelector("h4");
         if (unidadesDisponibles) {
@@ -265,6 +280,25 @@ function comprarCarrito() {
     renderizarCarrito(carrito);
     alert(`¡Compra realizada! Total a pagar: $${totalAPagar}`);
 }
+
+
+//FUNCION RELACIONADA A CARGAR DATOS DE PRODUCTOS 
+
+function cargarProductos() {
+    return fetch('productos.json')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('La solicitud no fue exitosa');
+        }
+        return response.json();
+      })
+      .catch(error => {
+        console.error('Error al obtener los datos de productos:', error);
+        throw error;
+      });
+  }
+
+
 
 
 
